e6cbb8620028b2a4d16cd47d1df3f3350e903584
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-24 00:08:36 +0700

    fix -python bakong-khqr

--- bakong_khqr_service/bakong.py
+++ bakong_khqr_service/bakong.py
@@ -1,24 +1,13 @@
-import os
 from dotenv import load_dotenv
-from bakong_khqr import KHQR
-
 load_dotenv()
 
-khqr = KHQR(os.getenv("BAKONG_TOKEN"))
+from fastapi import FastAPI
+from bakong_khqr_service.routes.qr_routes import router
 
-def generate_khqr(amount: int, order_id: str):
-    return khqr.create_qr(
-        bank_account=os.getenv("BANK_ACCOUNT"),
-        phone_number=os.getenv("BANK_PHONE_NUMBER"),
-        merchant_name=os.getenv("MERCHANT_NAME"),
-        merchant_city=os.getenv("MERCHANT_CITY"),
+app = FastAPI(title="Bakong KHQR Service")
 
-        amount=int(amount),           
-        currency="KHR",
+app.include_router(router)
 
-        store_label="UrbanStore",     # Brand name store
-        terminal_label="T1",          
-
-        bill_number=order_id,
-        static=False
-    )
+@app.get("/")
+def health():
+    return {"status": "Bakong service running"}
