994e9023153def3156031e128d8830a0de3f4b7b
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-23 10:17:19 +0700

    retry payment bakong endpoint

--- production-api/src/routes/payment.routes.js
+++ production-api/src/routes/payment.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { 
     createPayment, 
+    retryPayment
 } from "../controllers/payment.controller.js";
-
+import { protect } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
-router.post("/bakong", createPayment);
+router.post("/bakong", protect, createPayment);
+router.post("/bakong/retry", protect, retryPayment);
 
 export default router;
