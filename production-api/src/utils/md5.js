d1d53a7b9b0d8cff6511778d77544c52e4bd854f
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-27 22:47:25 +0700

    UPDATE Bakong production SECRET_KEY

--- production-api/src/utils/md5.js
+++ production-api/src/utils/md5.js
@@ -1,6 +1,12 @@
 import crypto from "crypto";
 
 export const generatePaymentHash = ({ orderId, amount, currency }) => {
-  const raw = `${orderId}|${amount}|${currency}|BAKONG`;
+  
+  /* Bakong production secret key */
+  const secretKey = process.env.BAKONG_SECRET_KEY;
+  if(!secretKey){
+    throw new Error("BAKONG_SECRET_KEY is not defined");
+  }
+  const raw = `${orderId}|${amount}|${currency}|${secretKey}`;
   return crypto.createHash("md5").update(raw).digest("hex");
 };
