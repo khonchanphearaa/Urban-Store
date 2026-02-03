96424e24c3d7fe8e19c50e5c7efb90914e2265c9
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-03 12:26:15 +0700

    route add checkStatus

--- production-api/src/routes/payment.routes.js
+++ production-api/src/routes/payment.routes.js
@@ -1,12 +1,10 @@
 import express from "express";
-import { 
-    createPayment, 
-    retryPayment
-} from "../controllers/payment.controller.js";
+import { createPayment, retryPayment, checkPaymentStatus} from "../controllers/payment.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/bakong", protect, createPayment);
 router.post("/bakong/retry", protect, retryPayment);
+router.post("/checkStatus",protect, checkPaymentStatus)
 
 export default router;
