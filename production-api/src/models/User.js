9130e3c20f70127c1b04205d047a07d37c5461d9
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-07 09:05:37 +0700

    Add field OTP

--- production-api/src/models/User.js
+++ production-api/src/models/User.js
@@ -4,8 +4,9 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, unique: true, required: true, lowercase: true },
   password: { type: String, required: true, select: false },
+  otp: {type: String},
+  otpExpires: { type: Date },
   role: { type: String, enum: ["USER", "ADMIN"], default: "USER" },
-
   refreshToken: { type: String }
 }, { timestamps: true });
 
