d7871f7f52a1d342abe6f40f092836d95d394168
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-10 08:59:51 +0700

    Update proxy seucrity check file secret key

--- bakong-khqr-proxy/src/index.ts
+++ bakong-khqr-proxy/src/index.ts
@@ -1,38 +1,41 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 
-const app = new Hono()
-
-// Use the secret you generated
-const PROXY_SECRET = "68d850e514267459d6cc90e8af92ea210ffc3b0379c70063c3e64f629042e78e"
-
+type Bindings = {
+  PROXY_SECRET: string
+}
+const app = new Hono<{ Bindings: Bindings }>();
 app.use('/*', cors())
 
 app.all('/bakong/*', async (c) => {
-  // 1. Security Check
+  const PROXY_SECRET = c.env.PROXY_SECRET
+  /* Security Check */
   if (c.req.header('X-Proxy-Auth') !== PROXY_SECRET) {
+    console.error("Auth Mismatch:", c.req.header('X-Proxy-Auth'), "vs", PROXY_SECRET);
     return c.json({ error: 'Unauthorized Proxy Access' }, 401)
   }
 
   const path = c.req.path.replace('/bakong', '')
   const targetUrl = `https://api-bakong.nbc.gov.kh${path}`
-  
-  // 2. Setup Stealth Headers to trick CloudFront
+
+  /* Setup Stealth Headers to trick CloudFront */
   const headers = new Headers()
   headers.set('Host', 'api-bakong.nbc.gov.kh')
   headers.set('Accept', 'application/json')
   headers.set('Content-Type', 'application/json')
   headers.set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36')
 
-  // Forward the Bakong Token from Python
+  /* Forward the Bakong Token from Python */
   const auth = c.req.header('Authorization')
   if (auth) headers.set('Authorization', auth)
 
   try {
+    const body = c.req.method !== 'GET' && c.req.method !== 'HEAD' ? await c.req.text() : undefined
+
     const response = await fetch(targetUrl, {
       method: c.req.method,
       headers: headers,
-      body: c.req.method !== 'GET' ? await c.req.text() : undefined
+      body: body
     })
 
     return new Response(response.body, response)
