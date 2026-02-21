5c6ed60021a881d59e37c6628bdd24c27e3576eb
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-22 02:01:11 +0700

    add routes wiishlist

--- production-api/src/routes/product.routes.js
+++ production-api/src/routes/product.routes.js
@@ -8,6 +8,8 @@ import {
   updateProduct,
   deleteProduct,
   addStock,
+  toggleWishlist,
+  getWishlist
 } from "../controllers/product.controller.js";
 
 import { upload } from "../middlewares/upload.js"; // multer + Cloudinary storage
@@ -16,13 +18,16 @@ const router = express.Router();
 
 /*  Public Routes (USER With ADMIN) */
 router.get("/", getProducts);
+router.get("/wishlist", protect, getWishlist);
 router.get("/:id", getProductById);
+
+/* Authenticated User Routes */
+router.post("/:id/wishlist", protect, toggleWishlist);
 
 /* Admin Only Routes */
 router.post("/", protect, isAdmin, upload.array("images", 5), createProduct);
 router.put("/:id", protect, isAdmin, upload.array("images", 5), updateProduct);
 router.post("/:id/add-stock", protect, isAdmin, addStock); 
 router.delete("/:id", protect, isAdmin, deleteProduct);
-
 
 export default router;
