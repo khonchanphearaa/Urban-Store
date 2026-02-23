906d27bddcb24e804d84aa12856dd453a1bf9c67
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-24 00:28:30 +0700

    add route controll by order admin

--- production-api/src/routes/order.routes.js
+++ production-api/src/routes/order.routes.js
@@ -5,9 +5,13 @@ import { isUser } from "../middlewares/user.middleware.js";
 import {
   createOrder,
   getUserOrders,
-  getOrderById,
-  updateOrderStatus
+  getOrderById
 } from "../controllers/order.controller.js";
+import {
+  getAllOrders,
+  getOrderByIdAdmin,
+  updateOrderStatus,
+} from "../controllers/admin.order.controller.js";
 
 const router = express.Router();
 
@@ -17,8 +21,12 @@ router.use(protect);
 // User routes
 router.post("/",isUser, createOrder);
 router.get("/", isUser, getUserOrders);
-router.get("/:id", isUser, getOrderById);
 
-// Admin route to update status
-router.put("/:id", isAdmin, updateOrderStatus);
+// Admin routes
+router.get("/admin", isAdmin, getAllOrders);
+router.get("/admin/:id", isAdmin, getOrderByIdAdmin);
+router.put("/admin/:id", isAdmin, updateOrderStatus);
+
+// User route by id
+router.get("/:id", isUser, getOrderById);
 export default router;
